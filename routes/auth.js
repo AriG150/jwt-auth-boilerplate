49af37aeaf78b66a6ef177d3ae59f1bba1774b05
Ari <gryphon150@gmail.com>
2019-11-05 13:48:39 -0800

    POST /auth/signup complete

--- routes/auth.js
+++ routes/auth.js
@@ -5,17 +5,59 @@ const User = require('../models/user');
 
 //POST /auth/signup
 router.post('/signup', (req, res) => {
-  res.send("hit the signup route");
+  //See if the email is already in the db 
+  User.findOne({ email: req.body.email }, (err, user) => {
+    //if yes, return error
+    if (user) {
+      res.json({
+        type: 'error',
+        message: 'Email already in database'
+      })
+    } else {
+      //if no, create the user in the db 
+      let user = new User(req.body);
+      user.save((err, user) => {
+        if(err) {
+          res.json({
+            type: 'error',
+            message: 'Database error creating user',
+            error: err
+          })
+        } else {
+          //sign a token
+          const token = jwt.sign(user.toObject(),process.env.JWT_SECRET, {
+            expiresIn: '1d'
+          });
+          //return the token
+          res.status(200).json({
+            type: 'sucess', 
+            user: user.toObject(),
+            token
+          })
+        }
+      })
+    }
+  })
 });
 
 //POST /auth/login
 router.post('/login', (req, res) => {
-  res.send("hit the login route");
+  //Find user in db
+    //if no user, return err 
+    //if user, check auth 
+      //if authenticated, sign a token 
+      // return the token 
 });
 
 //POST /auth/me/from/token 
 router.post('/me/from/token', (req, res) => {
-  res.send("hit the verification route");
+  //request must contain a token
+    //if not token, return error
+    //if token, verify it
+      //if errors during verification, return error (bad token)
+      //if token is valid, use token to look up user in db 
+        //if no user, return error
+        //if user, return user and token to front 
 });
 
 module.exports = router;
