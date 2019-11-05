966550cbd870d5017fcea70fdca7b694511b4087
Ari <gryphon150@gmail.com>
2019-11-05 11:59:41 -0800

    auth.js

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+//POST /auth/signup
+router.post('/signup', (req, res) => {
+  res.send("hit the signup route");
+});
+
+//POST /auth/login
+router.post('/login', (req, res) => {
+  res.send("hit the login route");
+});
+
+//POST /auth/me/from/token 
+router.post('/me/from/token', (req, res) => {
+  res.send("hit the verification route");
+});
+
+module.exports = router;
